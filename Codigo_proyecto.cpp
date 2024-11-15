1e225ad1f294c52a92224d97d42c6847b7faa39d
Juaneliz <A01708389@tec.mx>
2024-11-15 09:29:05 -0600

    Update Codigo_proyecto.cpp
     Cambio de variable y fclases para que respete los elementos del archivo .h

--- Codigo_proyecto.cpp
+++ Codigo_proyecto.cpp
@@ -3,34 +3,34 @@
 #include "Codigo_proyecto.h"
 
 int main(){
-    Tipo_pelicula terror("terror",155,2000);
+    Pelicula terror("terror",155,2000);
     std::cout<< terror.get_genero()<<std::endl;
     std::cout<< terror.get_duracion()<<std::endl;
-    std::cout<< terror.get_horario()<<std::endl;
+    std::cout<< terror.get_año()<<std::endl;
 
-    Tipo_pelicula romantica("romantica",120,1800);
+    Pelicula romantica("romantica",120,1800);
     std::cout<<"Pelicula: "<<romantica.get_genero()<<std::endl;
     std::cout<<"Duracion: "<<romantica.get_duracion()<<std::endl;
-    std::cout<<"Horario en horas militar: "<<romantica.get_horario()<<std::endl;
+    std::cout<<"Horario en horas militar: "<<romantica.get_año()<<std::endl;
 
-    Tipo_pelicula peli;
+    Pelicula peli;
     std::cout<< peli.get_genero()<<std::endl;
     std::cout<< peli.get_duracion()<<std::endl;
-    std::cout<< peli.get_horario()<<std::endl;
+    std::cout<< peli.get_año()<<std::endl;
     /*Fin de objetos de primera clase
 
     Inicio objetos segunda clase*/
-    Lugar_funcion Juriquilla("Juriquilla",89.2,3);
+    Cine Juriquilla("Juriquilla",89.2,3);
     std::cout<<"Lugar: "<<Juriquilla.get_lugar()<<std::endl;
     std::cout<<"Precio: "<<Juriquilla.get_precio()<<std::endl;
     std::cout<<"Boleto: "<<Juriquilla.get_boletos()<<std::endl;
     /* Final de objetos segunda clase
     
     Inicio objetos tercera clase*/
-    Pelicula Transformers("Transformers","Mark Wahlberg",2016);
+    Funciones Transformers("Transformers","Mark Wahlberg",2016);
     std::cout<<"Pelicula: "<<Transformers.get_pelicula()<<std::endl;
     std::cout<<"Actor: "<<Transformers.get_actor_princ()<<std::endl;
-    std::cout<<"año" <<Transformers.get_año()<<std::endl;
+    std::cout<<"año" <<Transformers.get_horario()<<std::endl;
 
 
 
