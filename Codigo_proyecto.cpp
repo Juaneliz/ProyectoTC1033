576af64491d7c71c65269f1c8499d222753fe30e
Juaneliz <A01708389@tec.mx>
2024-11-22 09:03:08 -0600

    Add files via upload
    Creacion de objetos a partir del diagrama y del codigo

--- Codigo_proyecto.cpp
+++ Codigo_proyecto.cpp
@@ -1,40 +1,24 @@
 #include <iostream>
 #include <string>
 #include "Codigo_proyecto.h"
+#include "Pelicula.h"
+#include "Funciones.h"
+using namespace std;
 
 int main(){
-    Pelicula terror("terror",155,2000);
-    std::cout<< terror.get_genero()<<std::endl;
-    std::cout<< terror.get_duracion()<<std::endl;
-    std::cout<< terror.get_año()<<std::endl;
+    Pelicula Soncomoninos("comedia",120,2015,"Adam Sandler","Zack Snyder");
+    cout<<Soncomoninos.get_genero()<<endl;
+    cout<<Soncomoninos.display_info()<<endl;
 
-    Pelicula romantica("romantica",120,1800);
-    std::cout<<"Pelicula: "<<romantica.get_genero()<<std::endl;
-    std::cout<<"Duracion: "<<romantica.get_duracion()<<std::endl;
-    std::cout<<"Horario en horas militar: "<<romantica.get_año()<<std::endl;
+    Funciones funcion2(Soncomoninos,"pg-13","ingles",8,"subtitulada en espaniol");
+    funcion2.display_infopeli();
 
-    Pelicula peli;
-    std::cout<< peli.get_genero()<<std::endl;
-    std::cout<< peli.get_duracion()<<std::endl;
-    std::cout<< peli.get_año()<<std::endl;
-    /*Fin de objetos de primera clase
+    Cine Antea(funcion2,175.4,5);
+    Antea.calcular_precio_total();
+    Antea.info_final();
 
-    Inicio objetos segunda clase*/
-    Cine Juriquilla("Juriquilla",89.2,3);
-    std::cout<<"Lugar: "<<Juriquilla.get_lugar()<<std::endl;
-    std::cout<<"Precio: "<<Juriquilla.get_precio()<<std::endl;
-    std::cout<<"Boleto: "<<Juriquilla.get_boletos()<<std::endl;
-    /* Final de objetos segunda clase
-    
-    Inicio objetos tercera clase*/
-    Funciones Transformers("Transformers","Mark Wahlberg",2016);
-    std::cout<<"Pelicula: "<<Transformers.get_pelicula()<<std::endl;
-    std::cout<<"Actor: "<<Transformers.get_actor_princ()<<std::endl;
-    std::cout<<"año" <<Transformers.get_horario()<<std::endl;
-
-
-
-
+    Esfera esfera(funcion2,4);
+    esfera.precio_viptotal();
 
     return 0;
 }
