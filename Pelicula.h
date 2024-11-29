93f09e5b2663c73da4a495adea0701f3275d9308
Juaneliz <A01708389@tec.mx>
2024-11-28 22:47:37 -0600

    Update Pelicula.h
    Se agrego comentarios y el uso del nombre para incluir en la pelicula

--- Pelicula.h
+++ Pelicula.h
@@ -2,74 +2,118 @@
 #define PELICULA_H_
 
 #include <iostream>
-#include<string>
+#include <string>
 
 using namespace std;
-class Pelicula{
-    private:
-    string genero;
-    int duracion;
-    int año;
-    string actor_princ;
-    string director;
-    public:
-    Pelicula();
-    Pelicula(string g, int d, int k,string ap, string di): genero(g), duracion(d) ,año(k) ,actor_princ(ap), director(di){};   
-    string get_genero();
-    int get_duracion();
-    int get_año();
-    string get_actor_princ();
-    string get_director();
-    string display_info();
 
-    void set_genero(string);
-    void set_duracion(int);
-    void set_año(int);
-    void set_actor_princ(string);
-    void set_director(string);
+class Pelicula {
+private:
+    string nombre;           // Nombre de la película
+    string genero;           // Género de la película
+    int duracion;            // Duración de la película en minutos
+    int año;                 // Año de estreno de la película
+    string actor_princ;      // Actor principal de la película
+    string director;         // Director de la película
+
+public:
+    // Constructor por defecto
+    Pelicula();
+    
+    // Constructor con parámetros
+    Pelicula(string n, string g, int d, int k, string ap, string di) 
+        : nombre(n), genero(g), duracion(d), año(k), actor_princ(ap), director(di) {}
+
+    // Métodos getter (accesores)
+    string get_nombre();        // Regresa el nombre de la película
+    string get_genero();        // Regresa el género de la película
+    int get_duracion();         // Regresa la duración de la película
+    int get_año();              // Regresa el año de estreno
+    string get_actor_princ();   // Regresa el actor principal
+    string get_director();      // Regresa el director de la película
+
+    // Métodos setter (mutadores)
+    void set_nombre(string);    // Establece el nombre de la película
+    void set_genero(string);    // Establece el género de la película
+    void set_duracion(int);     // Establece la duración de la película
+    void set_año(int);          // Establece el año de estreno
+    void set_actor_princ(string); // Establece el actor principal
+    void set_director(string);  // Establece el director de la película
+
+    // Método para mostrar toda la información de la película
+    string display_info();      // Regresa un string con toda la información de la película
 };
 
-Pelicula::Pelicula(){
-    genero="";
-    duracion=0;
-    año=0;
-    actor_princ="";
-    director="";
-}
-string Pelicula::get_genero(){
-    return genero;
-}
-int Pelicula:: get_duracion(){
-    return duracion;
-}
-int Pelicula::get_año(){
-    return año;
-}
-string Pelicula::get_actor_princ(){
-    return actor_princ;
-}
-string Pelicula::get_director(){
-    return director;
-}
- void Pelicula::set_genero(std::string g){
-    genero=g;
-}
-void Pelicula:: set_duracion(int d){
-    duracion=d;
-}
-void Pelicula::set_año(int k){
-    año=k;
-}
-void Pelicula::set_actor_princ(string ap){
-    actor_princ=ap;
-}
-void Pelicula::set_director(string di){
-    director=di;
-}
-string Pelicula::display_info(){
-return "Informacion de pelicula:"+genero+"Duracion"+
-    std::to_string(duracion)+"Anio:"+std::to_string(año)+
-    actor_princ+"Director:"+director;
+// Implementación de los métodos
 
+// Constructor por defecto
+Pelicula::Pelicula() {
+    nombre = "";
+    genero = "";
+    duracion = 0;
+    año = 0;
+    actor_princ = "";
+    director = "";
 }
+
+// Métodos getter (accesores)
+string Pelicula::get_nombre() {
+    return nombre;  // Retorna el nombre de la película
+}
+
+string Pelicula::get_genero() {
+    return genero;  // Retorna el género de la película
+}
+
+int Pelicula::get_duracion() {
+    return duracion;  // Retorna la duración de la película en minutos
+}
+
+int Pelicula::get_año() {
+    return año;  // Retorna el año de estreno de la película
+}
+
+string Pelicula::get_actor_princ() {
+    return actor_princ;  // Retorna el nombre del actor principal
+}
+
+string Pelicula::get_director() {
+    return director;  // Retorna el nombre del director de la película
+}
+
+// Métodos setter (mutadores)
+void Pelicula::set_nombre(string n) {
+    nombre = n;  // Establece el nombre de la película
+}
+
+void Pelicula::set_genero(string g) {
+    genero = g;  // Establece el género de la película
+}
+
+void Pelicula::set_duracion(int d) {
+    duracion = d;  // Establece la duración de la película
+}
+
+void Pelicula::set_año(int k) {
+    año = k;  // Establece el año de estreno
+}
+
+void Pelicula::set_actor_princ(string ap) {
+    actor_princ = ap;  // Establece el nombre del actor principal
+}
+
+void Pelicula::set_director(string di) {
+    director = di;  // Establece el nombre del director
+}
+
+// Método para mostrar toda la información de la película
+string Pelicula::display_info() {
+    // Retorna un string concatenado con toda la información de la película
+    return "Información de película: " + nombre + " " +
+           "Género: " + genero + " " +
+           "Duración: " + std::to_string(duracion) + " " +
+           "Año: " + std::to_string(año) + " " +
+           "Actor principal: " + actor_princ + " " +
+           "Director: " + director;
+}
+
 #endif
