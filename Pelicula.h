a1f00e58cc37904a4e84bc2c82ae299cc5ff1a68
Juaneliz <A01708389@tec.mx>
2024-11-30 17:41:53 -0600

    Add files via upload

--- Pelicula.h
+++ Pelicula.h
@@ -8,42 +8,42 @@ using namespace std;
 
 class Pelicula {
 private:
-    string nombre;           // Nombre de la película
-    string genero;           // Género de la película
-    int duracion;            // Duración de la película en minutos
-    int anio;                 // Año de estreno de la película
-    string actor_princ;      // Actor principal de la película
-    string director;         // Director de la película
+    string nombre;           // Nombre de la pelicula
+    string genero;           // Género de la pelicula
+    int duracion;            // Duracion de la pelicula en minutos
+    int anio;                 // Anio de estreno de la pelicula
+    string actor_princ;      // Actor principal de la pelicula
+    string director;         // Director de la pelicula
 
 public:
     // Constructor por defecto
     Pelicula();
     
-    // Constructor con parámetros
+    // Constructor con parametros
     Pelicula(string n, string g, int d, int k, string ap, string di) 
         : nombre(n), genero(g), duracion(d), anio(k), actor_princ(ap), director(di) {}
 
-    // Métodos getter (accesores)
-    string get_nombre();        // Regresa el nombre de la película
-    string get_genero();        // Regresa el género de la película
-    int get_duracion();         // Regresa la duración de la película
-    int get_anio();              // Regresa el año de estreno
+    // Metodos getter (accesores)
+    string get_nombre();        // Regresa el nombre de la pelicula
+    string get_genero();        // Regresa el genero de la pelicula
+    int get_duracion();         // Regresa la duracion de la pelicula
+    int get_anio();              // Regresa el anio de estreno
     string get_actor_princ();   // Regresa el actor principal
-    string get_director();      // Regresa el director de la película
+    string get_director();      // Regresa el director de la pelicula
 
-    // Métodos setter (mutadores)
-    void set_nombre(string);    // Establece el nombre de la película
-    void set_genero(string);    // Establece el género de la película
-    void set_duracion(int);     // Establece la duración de la película
-    void set_anio(int);          // Establece el año de estreno
+    // Metodos setter (mutadores)
+    void set_nombre(string);    // Establece el nombre de la pelicula
+    void set_genero(string);    // Establece el genero de la pelicula
+    void set_duracion(int);     // Establece la duracion de la pelicula
+    void set_anio(int);          // Establece el anio de estreno
     void set_actor_princ(string); // Establece el actor principal
-    void set_director(string);  // Establece el director de la película
+    void set_director(string);  // Establece el director de la pelicula
 
-    // Método para mostrar toda la información de la película
-    string display_info();      // Regresa un string con toda la información de la película
+    // Metodo para mostrar toda la informacion de la pelicula
+    string display_info();      // Regresa un string con toda la informacion de la pelicula
 };
 
-// Implementación de los métodos
+// Implementacion de los metodos
 
 // Constructor por defecto
 Pelicula::Pelicula() {
@@ -55,21 +55,21 @@ Pelicula::Pelicula() {
     director = "";
 }
 
-// Métodos getter (accesores)
+// Metodos getter (accesores)
 string Pelicula::get_nombre() {
-    return nombre;  // Retorna el nombre de la película
+    return nombre;  // Retorna el nombre de la pelicula
 }
 
 string Pelicula::get_genero() {
-    return genero;  // Retorna el género de la película
+    return genero;  // Retorna el genero de la pelicula
 }
 
 int Pelicula::get_duracion() {
-    return duracion;  // Retorna la duración de la película en minutos
+    return duracion;  // Retorna la duracion de la pelicula en minutos
 }
 
 int Pelicula::get_anio() {
-    return anio;  // Retorna el año de estreno de la película
+    return anio;  // Retorna el anio de estreno de la pelicula
 }
 
 string Pelicula::get_actor_princ() {
@@ -77,24 +77,24 @@ string Pelicula::get_actor_princ() {
 }
 
 string Pelicula::get_director() {
-    return director;  // Retorna el nombre del director de la película
+    return director;  // Retorna el nombre del director de la pelicula
 }
 
-// Métodos setter (mutadores)
+// Metodos setter (mutadores)
 void Pelicula::set_nombre(string n) {
-    nombre = n;  // Establece el nombre de la película
+    nombre = n;  // Establece el nombre de la pelicula
 }
 
 void Pelicula::set_genero(string g) {
-    genero = g;  // Establece el género de la película
+    genero = g;  // Establece el genero de la pelicula
 }
 
 void Pelicula::set_duracion(int d) {
-    duracion = d;  // Establece la duración de la película
+    duracion = d;  // Establece la duracion de la pelicula
 }
 
 void Pelicula::set_anio(int k) {
-    anio = k;  // Establece el año de estreno
+    anio = k;  // Establece el anio de estreno
 }
 
 void Pelicula::set_actor_princ(string ap) {
@@ -105,13 +105,13 @@ void Pelicula::set_director(string di) {
     director = di;  // Establece el nombre del director
 }
 
-// Método para mostrar toda la información de la película
+// Metodo para mostrar toda la informacion de la pelicula
 string Pelicula::display_info() {
-    // Retorna un string concatenado con toda la información de la película
-    return "Información de película: " + nombre + " " +
-           "Género: " + genero + " " +
-           "Duración: " + std::to_string(duracion) + " " +
-           "Año: " + std::to_string(anio) + " " +
+    // Retorna un string concatenado con toda la informacion de la pelicula
+    return "Informacion de pelicula: " + nombre + " " +
+           "Genero: " + genero + " " +
+           "Duracion: " + std::to_string(duracion) + " " +
+           "Anioo: " + std::to_string(anio) + " " +
            "Actor principal: " + actor_princ + " " +
            "Director: " + director;
 }
